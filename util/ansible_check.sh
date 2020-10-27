01127ed9261ec6630486cac81f8f8124d934e677
Envel-Nikita-Gutsenkov <52075465+Envel-Nikita-Gutsenkov@users.noreply.github.com>
2020-10-27 19:09:18 +0300

    fix errors

--- util/ansible_check.sh
+++ util/ansible_check.sh
@@ -13,14 +13,4 @@ Streisand requires Ansible and it is not installed.
 Please see the README Installation section on Prerequisites"
     exit 1
   fi
-
-  ansible_version="$(ansible --version | head -1 | grep -oe '2[.0-9]*')"
-
-  if ! ./util/version_at_least.py "$REQUIRED_ANSIBLE_VERSION" "$ansible_version" ; then
-      echo "
-Streisand requires Ansible version $REQUIRED_ANSIBLE_VERSION or higher.
-This system has Ansible $ansible_version.
-"
-      exit 1
-  fi
 }
